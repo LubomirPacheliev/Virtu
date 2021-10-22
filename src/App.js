70a9e0284953721217547e4a132c50ed3202a05c
LubomirP <lubomirpacheliev@gmail.com>
2021-10-22 12:44:38 +0300

    Order History Empty Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import Chart from "./components/Chart.js";
+import OrderHistory from "./components/OrderHistory.js";
 import './style.css';
 
 const App = () => {
   return (
     <div className="App">
       <Chart widgetProps={{
-                "autosize": true,
-                "symbol": "BINANCE:BTCUSDT",
-                "interval": "60",
-                "timezone": "Etc/UTC",
-                "theme": "dark",
-                "style": "1",
-                "locale": "en",
-                "toolbar_bg": "#f1f3f6",
-                "enable_publishing": false,
-                "allow_symbol_change": true,
-                "container_id": "tradingview_82f39"
-            }} />
+        "autosize": true,
+        "symbol": "BINANCE:BTCUSDT",
+        "interval": "60",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "toolbar_bg": "#f1f3f6",
+        "enable_publishing": false,
+        "allow_symbol_change": true,
+        "container_id": "tradingview_82f39"
+      }} />
+      <OrderHistory />
     </div>
   );
 }
---
+++ src/components/OrderHistory.js
@@ -0,0 +1,14 @@
+const OrderHistory = props => {
+    return ( 
+        <article className="order-history">
+            <div className="order-history-nav">
+                <h1>Order History</h1>
+            </div>
+            <div className="order-history-list">
+                
+            </div>
+        </article>
+    );
+}
+ 
+export default OrderHistory;

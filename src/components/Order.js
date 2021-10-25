e2a43dfdd2cf732e01bb4fdb5125bf99d2803e27
LubomirP <lubomirpacheliev@gmail.com>
2021-10-25 14:14:23 +0300

    Tabs and colored buttons

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,11 +2,25 @@ import { useState } from "react";
 
 const Order = props => {
     const [orderType, setOrderType] = useState('buy');
+
+    const clickOrderBtn = (orderType) => {
+        const sell = document.querySelector('#sell');
+        const buy = document.querySelector('#buy');
+        if (orderType === 'buy') {
+            sell.style.color = 'white';
+            buy.style.color = '#26A69A';
+        } else {
+            sell.style.color = '#EF5350';
+            buy.style.color = 'white';
+        }
+        setOrderType(orderType);
+    }
+
     return (
         <article className="order">
             <ul>
-                <li onClick={() => setOrderType('buy')}>Buy</li>
-                <li onClick={() => setOrderType('sell')}>Sell</li>
+                <li onClick={() => clickOrderBtn('buy')} id="buy">BUY</li>
+                <li onClick={() => clickOrderBtn('sell')} id="sell">SELL</li>
             </ul>
             <OrderForm orderType={orderType} />
         </article>
@@ -16,15 +30,13 @@ const Order = props => {
 const OrderForm = props => {
     const orderType = props.orderType;
     return (
-        <form className="order-form">
-            <p>Available: 50USDT</p>
-            <label htmlFor="price">At Price</label>
-            <input type="text" id="price" name="price" />
-            <label htmlFor="amount">Amount</label>
-            <input type="text" id="amount" name="amount" />
+        <div className="order-form">
+            <p>available: 50 USDT</p>
+            <input type="text" id="price" name="price" placeholder="at price:" />
+            <input type="text" id="amount" name="amount" placeholder="amount:" />
             {orderType === 'buy' && <button className="btn-buy">Buy</button>}
             {orderType === 'sell' && <button className="btn-sell">Sell</button>}
-        </form>
+        </div>
     );
 }
  

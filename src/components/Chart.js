d7d91790f576d396980b4f51480c55fbf0247077
LubomirP <lubomirpacheliev@gmail.com>
2021-10-27 15:05:25 +0300

    embedding the TV widgets on my own

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,11 +1,55 @@
-import { AdvancedChart } from "react-tradingview-embed";
+import React, {useEffect, useRef} from 'react';
 
 const Chart = props => {
+    const ref = useRef();
+    const { widgetProps } = props;
+
+    useEffect(() => {
+      let refValue;
+
+      const script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = 'https://s3.tradingview.com/tv.js';
+      script.async = true;
+      script.onload = () => {
+        if (typeof TradingView != 'undefined') {
+          new window.TradingView.widget({
+            "width": "100%",
+            "symbol": "BINANCE:BTCUSDT",
+            "interval": "1h",
+            "range": "1M",
+            "timezone": "Etc/UTC",
+            "theme": "dark",
+            "style": "9",
+            "locale": "en",
+            "toolbar_bg": "rgba(0, 0, 0, 0.8)",
+            "hide_top_toolbar": false,
+            "hide_side_toolbar": false,
+            "withdateranges": true,
+            "save_image": true,
+            "enable_publishing": false,
+            "container_id": 'tradingview_82f39',
+            ...widgetProps
+          });
+        } 
+      };
+
+      ref.current.appendChild(script);
+      refValue = ref.current;
+
+      return () => {
+          if (refValue) {
+            while (refValue.firstChild) {
+              refValue.removeChild(refValue.firstChild);
+          }
+        }
+      }
+    }, [widgetProps]);
+
     return ( 
-        <article className="chart" id="tradingview_82f39">
-            <AdvancedChart widgetProps={props.widgetProps} />
+        <article className="chart" id="tradingview_82f39" ref={ref}>
         </article>
     );
 }
- 
+
 export default Chart;
--- src/components/TA.js
+++ src/components/TA.js
@@ -1,22 +1,51 @@
-import { TechnicalAnalysis } from "react-tradingview-embed";
+import React, { useEffect, useRef } from 'react';
 
-const TA = props => {
+const TA = () => {
+
     return (
         <article className="technical-analysis">
-            <TechnicalAnalysis widgetProps={
-                {
-                    "interval": "1h",
-                    "width": "100%",
-                    "isTransparent": false,
-                    "height": "100%",
-                    "symbol": "BINANCE:BTCUSDT",
-                    "showIntervalTabs": true,
-                    "locale": "en",
-                    "colorTheme": "dark" 
-                }
-            } />
+            <TechnicalAnalysisWidget />
         </article>
     );
 }
  
+const TechnicalAnalysisWidget = props => {
+    const { widgetProps } = props;
+    const ref = useRef();
+
+    useEffect(() => {
+        let refValue;
+
+        const script = document.createElement('script');
+        script.src = "https://s3.tradingview.com/external-embedding/"
+        + "embed-widget-technical-analysis.js";
+        script.async = true;
+        script.type = "text/javascript";
+        script.innerHTML = JSON.stringify({
+            "interval": "1h",
+            "width": "100%",
+            "isTransparent": false,
+            "height": "100%",
+            "symbol": "BINANCE:BTCUSDT",
+            "showIntervalTabs": true,
+            "locale": "en",
+            "colorTheme": "dark",
+            ...widgetProps
+        });
+        
+        ref.current.appendChild(script);
+        refValue = ref.current;
+
+        return () => {
+            if (refValue) {
+              while (refValue.firstChild) {
+                refValue.removeChild(refValue.firstChild);
+              }
+            }
+          }
+    }, [widgetProps])
+
+    return <div ref={ref} />;
+}
+
 export default TA;

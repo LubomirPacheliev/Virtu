7d7e59cbabfefaab48fae13d077c174ebb2041c3
LubomirP <lubomirpacheliev@gmail.com>
2021-10-27 16:16:28 +0300

    navigation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
 import TradePair from "./components/TradePair.js";
 import './style.css';
 
 const App = () => {
   return (
-    <TradePair />
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/home">Home</Link></li>
+          <li><Link to="/market">Market</Link></li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/market" component={ TradePair } />
+        <Route path="/home" />
+      </Switch>
+    </Router>
   );
 }
 

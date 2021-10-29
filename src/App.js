abc75042b7304b43ad834fbbc04eac527956f1d4
LubomirP <lubomirpacheliev@gmail.com>
2021-10-29 14:40:41 +0300

    making a specific directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
-import TradePair from "./components/TradePair.js";
+import TradePair from "./components/marketDetails/TradePair.js";
 import './style.scss';
 
 const App = () => {

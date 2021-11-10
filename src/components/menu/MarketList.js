00e2920ee47645961a923a41f649296c11a5f50e
LubomirP <lubomirpacheliev@gmail.com>
2021-11-10 16:36:30 +0200

    only getting the data once in the market list

--- src/components/menu/MarketList.js
+++ src/components/menu/MarketList.js
@@ -6,19 +6,22 @@ const MarketList = props => {
     return (
         <div className="market-list">
             <ul>
-                {tickers.map((ticker, i) => <li key={i}>{`${ticker.c}`}</li>)}
+                {tickers.map((ticker, i) => <li key={i}>{`${ticker.s}: ${ticker.c}`}</li>)}
             </ul>
         </div>
     );
 }
 
-const getTickers = setTickers => {
+const getTickers = async setTickers => {
+    // We can't just use a fetch request here because of CORS, we either have to 
+    // use a proxy(slow) or make an entire back end for one request(not ideal either)
     const socket = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
     socket.onopen = () => console.log('connection established.');
     socket.onmessage = msg => {
         const tickers = JSON.parse(msg.data);
         const sortedTickers = tickers.sort((tickA, tickB) => Number(tickB.c) - Number(tickA.c));
         setTickers(sortedTickers);
+        socket.close();
     }
 }
 

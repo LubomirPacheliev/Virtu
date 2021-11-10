f080c7f20cc7bbbe28ae314936ef10ea28de5bce
LubomirP <lubomirpacheliev@gmail.com>
2021-11-10 15:00:52 +0200

    better naming

--- src/components/menu/MarketList.js
+++ src/components/menu/MarketList.js
@@ -2,22 +2,21 @@ import React, { useEffect, useState } from 'react';
 
 const MarketList = props => {
     const [tickers, setTickers] = useState([]);
-    useEffect(() => getWidget(setTickers), [ setTickers ]);
+    useEffect(() => getTickers(setTickers), [ setTickers ]);
     return (
         <div className="market-list">
             <ul>
-                {tickers.map((ticker, i) => <li key={i}>{`${ticker}`}</li>)}
+                {tickers.map((ticker, i) => <li key={i}>{`${ticker.c}`}</li>)}
             </ul>
         </div>
     );
 }
 
-const getWidget = setTickers => {
+const getTickers = setTickers => {
     const socket = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
     socket.onopen = () => console.log('connection established.');
     socket.onmessage = msg => {
         const parsed_msg = JSON.parse(msg.data);
-        console.log(parsed_msg);
         setTickers(parsed_msg);
     }
 }

a9d0f2daf377eeae8539941354450622766e857a
LubomirP <lubomirpacheliev@gmail.com>
2021-11-10 14:15:12 +0200

    establishing a connection to the ws server on binance

--- src/components/menu/MarketList.js
+++ src/components/menu/MarketList.js
@@ -1,35 +1,18 @@
 import React, { useRef, useEffect } from 'react';
-// import { useHistory } from 'react-router';
 
 const MarketList = props => {
-    const { widgetProps } = props;
     const ref = useRef();
-
-    // jeez this hook is amazing, it can act as both componentDidMount and componentDidUpdate
-    useEffect(() => getWidget(widgetProps, ref), [ widgetProps ]);
-
+    useEffect(() => getWidget(), [ ]);
     return (
         <div ref={ref} className="market-list"></div>
     );
 }
 
-const getWidget = (widgetProps, ref) => {
-    const script = document.createElement('script');
-    script.async = true;
-    script.type = 'text/javascript';
-    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-screener.js";
-    script.innerHTML = JSON.stringify({
-        "width": "100%",
-        "height": "100%",
-        "defaultColumn": "overview",
-        "screener_type": "crypto_mkt",
-        "displayCurrency": "USD",
-        "colorTheme": "dark",
-        "locale": "en",
-        ...widgetProps
-    });
-
-    ref.current.appendChild(script);
+const getWidget = () => {
+    const socket = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
+    socket.onopen = function(event) {
+        console.log('ig it works!');
+    }
 }
 
 export default MarketList;

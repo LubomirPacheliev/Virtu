8ccb2cb2476e6470db8680f0eaaec32726c9b2f7
LubomirP <lubomirpacheliev@gmail.com>
2021-11-10 14:53:09 +0200

    listing all the data

--- src/components/menu/MarketList.js
+++ src/components/menu/MarketList.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from 'react';
 
 const MarketList = props => {
-    const [data, setData] = useState([]);
-    useEffect(() => getWidget(setData), [ setData ]);
+    const [tickers, setTickers] = useState([]);
+    useEffect(() => getWidget(setTickers), [ setTickers ]);
     return (
-        <div className="market-list"></div>
+        <div className="market-list">
+            <ul>
+                {tickers.map((ticker, i) => <li key={i}>{`${ticker}`}</li>)}
+            </ul>
+        </div>
     );
 }
 
-const getWidget = setData => {
+const getWidget = setTickers => {
     const socket = new WebSocket('wss://stream.binance.com:9443/ws/!ticker@arr');
     socket.onopen = () => console.log('connection established.');
-    socket.onmessage = async msg => {
-        const parsed_msg = await JSON.parse(msg.data);
-        setData(parsed_msg);
+    socket.onmessage = msg => {
+        const parsed_msg = JSON.parse(msg.data);
+        console.log(parsed_msg);
+        setTickers(parsed_msg);
     }
 }
 

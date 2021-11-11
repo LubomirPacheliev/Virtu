75839100d30cf8b7ba67e99aef60484862617232
LubomirP <lubomirpacheliev@gmail.com>
2021-11-11 13:44:43 +0200

    grouping the tickers so I can make tabs

--- src/components/menu/MainList.js
+++ src/components/menu/MainList.js
@@ -2,10 +2,24 @@ import React from 'react';
 import Ticker from './Ticker.js';
 
 const MainList = props => {
+    const {tickers} = props;
+    const groups = tickers.length !== 0 && new Array(Math.floor(tickers.length / 10));
+    if (groups) {
+        groups.fill(new Array(10));
+        let i = 0;
+        groups.forEach(group => {
+            for (let j = 0; j < 10; j++) {
+                group[j] = tickers[i];
+                i++;
+            }
+        });
+        console.log(groups);
+    }
+
     return (
         <div className="main-list-component">
             <ul>
-                {props.tickers.map((ticker, i) => <Ticker key={i} ticker={ticker} />)}
+                {tickers.map((ticker, i) => <Ticker key={i} ticker={ticker} />)}
             </ul>
         </div>
     );

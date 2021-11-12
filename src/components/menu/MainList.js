9ba67da3523918b97e58e58513de71df2d8ac9c5
LubomirP <lubomirpacheliev@gmail.com>
2021-11-12 10:53:52 +0200

    not working piece of code ( need it stored somewhere )

--- src/components/menu/MainList.js
+++ src/components/menu/MainList.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Ticker from './Ticker.js';
 
 const MainList = props => {
     const {tickers} = props;
-    let groups = [];
-    if (tickers.length !== 0) {
-        groups = new Array(Math.floor(tickers.length / 10));
-        groups.fill(new Array(10));
-        let i = 0;
-        groups.forEach(group => {
-            for (let j = 0; j < 10; j++) {
-                group[j] = tickers[i];
-                i++;
-            }
-        });
-        console.log(groups);
-    }
+    const [openTab, setOpenTab] = useState(0);
+    const [groups, setGroups] = useState([]);
+
+    useEffect(() => {
+        let groups;
+        if (tickers.length !== 0) {
+            groups = new Array(Math.floor(tickers.length / 10));
+            groups.fill(new Array(10));
+            let i = 0;
+            groups.forEach(group => {
+                for (let j = 0; j < 10; j++) {
+                    group[j] = tickers[i];
+                    i++;
+                }
+            });
+            setGroups(groups);
+        }
+    }, [ tickers ]);
 
     return (
         <div className="main-list-component">
             <ul>
-                {tickers.map((ticker, i) => <Ticker key={i} ticker={ticker} />)}
+                {groups.map((group, i) => <p key={i} onClick={() => setOpenTab(i)}>Group {i + 1}</p>)}
+                {groups[openTab] === undefined || groups[openTab].map((ticker, i) => <Ticker key={i} ticker={ticker} />)}
             </ul>
-            {groups.map((group, i) => <p key={i} >{i + 1}</p>)}
         </div>
     );
 }

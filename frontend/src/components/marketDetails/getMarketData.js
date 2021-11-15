054684c89c45e61a7494ac4a3a05ebe5579aefd7
LubomirP <lubomirpacheliev@gmail.com>
2021-11-15 12:49:10 +0200

    fetch req to api

--- frontend/src/components/marketDetails/getMarketData.js
+++ frontend/src/components/marketDetails/getMarketData.js
@@ -1,3 +1,6 @@
-export const getMarketData = (symbol, ref) => {
-    
+import fetch from 'node-fetch';
+export const getMarketData = async (symbol, ref) => {
+    const url = `https://localhost:5000/api/order/${symbol}`;
+    const data = await fetch(url);
+    return await data.json();
 }

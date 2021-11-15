24f9933140ec98ba664c53c3c0c234ab5e7f2daa
LubomirP <lubomirpacheliev@gmail.com>
2021-11-15 12:38:57 +0200

    request handler for the market list & err handling

--- backend/main.js
+++ backend/main.js
@@ -6,9 +6,22 @@ const binance = 'https://api.binance.com';
 app.get('/api/order/:symbol', async (req, res) => {
     const symbol = req.params.symbol;
     const url = `${binance}/api/v3/ticker/24hr?symbol=${symbol}`;
-    const res = await fetch(url);
-    const parsed = await res.json();
-    res.send(parsed);
+    const data = await fetch(url);
+    if (!data.ok) console.log('Something went wrong.');
+    const parsed = await data.json();
+    res.status(200).send(parsed);
+});
+
+app.get('/api/markets', async (req, res) => {
+    const url = `${binance}/api/v3/ticker/24hr`;
+    try {
+        const data = await fetch(url);
+        const parsed = await data.json();
+        res.status(200).send(parsed);
+    } catch(e) {
+        console.error(e);
+        res.send();
+    }
 });
 
 app.listen(port, console.log(`sksksksk on: ${port}`));

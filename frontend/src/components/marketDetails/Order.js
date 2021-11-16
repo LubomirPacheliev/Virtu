073541d04af5719475d4fd0b6bb0ed004a30edc5
LubomirP <lubomirpacheliev@gmail.com>
2021-11-16 19:19:20 +0200

    puttng the ws in useEffect

--- frontend/src/components/marketDetails/Order.js
+++ frontend/src/components/marketDetails/Order.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router";
 
 const Order = props => {
     const [orderType, setOrderType] = useState('buy');
@@ -30,10 +31,21 @@ const Order = props => {
 
 const OrderForm = props => {
     const {orderType, portfolio} = props;
+    const [currPrice, setCurrPrice] = useState(0);
+    const { symbol } = useParams();
+
+    useEffect(() => {
+        const socket = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@ticker`);
+        socket.onmessage = msg => {
+            const data = JSON.parse(msg.data);
+            setCurrPrice(data.c); 
+        }
+    }, [setCurrPrice]);
+
     return (
         <div className="order-form">
             <p>available: {portfolio[0].amount} {portfolio[0].symbol} </p>
-            <input type="text" placeholder="at price:" />
+            <input type="text" placeholder="at price:" value={Number(currPrice).toFixed(2)} />
             <input type="text" placeholder="amount:" />
             <input type="text" placeholder="receive: " />
             {orderType === 'buy' && <button className="btn-buy">Buy</button>}

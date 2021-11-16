2150512879293108d7d5db8cb1d1b38bf75f2518
LubomirP <lubomirpacheliev@gmail.com>
2021-11-16 20:00:55 +0200

    displaying the price and receive amount

--- frontend/src/components/marketDetails/Order.js
+++ frontend/src/components/marketDetails/Order.js
@@ -31,23 +31,22 @@ const Order = props => {
 
 const OrderForm = props => {
     const {orderType, portfolio} = props;
-    const [currPrice, setCurrPrice] = useState(0);
+    const [currPrice, setCurrPrice] = useState();
     const { symbol } = useParams();
 
     useEffect(() => {
         const socket = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@ticker`);
         socket.onmessage = msg => {
             const data = JSON.parse(msg.data);
-            setCurrPrice(data.c); 
+            setCurrPrice(Number(data.c).toFixed(2));
         }
     }, [setCurrPrice]);
 
     return (
         <div className="order-form">
             <p>available: {portfolio[0].amount} {portfolio[0].symbol} </p>
-            <input type="text" placeholder="at price:" value={Number(currPrice).toFixed(2)} />
-            <input type="text" placeholder="amount:" />
-            <input type="text" placeholder="receive: " />
+            <input type="text" value={currPrice} />
+            <input type="text" value={ (portfolio[0].amount / currPrice) } />
             {orderType === 'buy' && <button className="btn-buy">Buy</button>}
             {orderType === 'sell' && <button className="btn-sell">Sell</button>}
         </div>

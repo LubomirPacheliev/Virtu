6fb9b95e69046a977bfe4f5e9753e038d5d4fa16
LubomirP <lubomirpacheliev@gmail.com>
2021-11-21 13:30:24 +0200

    two way binding for price and amount

--- frontend/src/components/marketDetails/OrderForm.js
+++ frontend/src/components/marketDetails/OrderForm.js
@@ -5,31 +5,41 @@ const OrderForm = props => {
     const { orderType } = props;
     const { portfolio } = props.portfolio;
     const {history, setHistory} = props.history;
+
     const [currPrice, setCurrPrice] = useState(1);
-    const [isInputting, setIsInputting] = useState(false);
+    const [currAmount, setCurrAmount] = useState(1000);
+    const [currCost, setCurrCost] = useState(1000);
+
     const { symbol } = useParams();
 
     useEffect(() => {
         const socket = new WebSocket(`wss://stream.binance.com:9443/ws/${symbol.toLowerCase()}@ticker`);
+        let i = 0;
         socket.onmessage = msg => {
             const data = JSON.parse(msg.data);
-            if (!isInputting) {
-                console.log('we setting the price here')
+            if (++i <= 1) {
                 setCurrPrice(Number(data.c).toFixed(2));
-            } else {
-                console.log('wat?') // KO?!
+                setCurrAmount(portfolio[0].amount / Number(data.c));
             }
         }
-    }, [setCurrPrice, isInputting]);
+    }, [symbol]);
 
     return (
         <div className="order-form">
             <p>available: {portfolio[0].amount} {portfolio[0].symbol} </p>
-            {/* <label for="at-price">at price: </label> */}
-            <input type="text" name="at-price" value={currPrice} onFocus={() => setIsInputting(true)} />
-            {/* <label for="receive">receive:</label> */}
-            <input type="text" name="receive" value={ (portfolio[0].amount / currPrice) } />
-            Cost: {currPrice * (portfolio[0].amount / currPrice)}
+            <input type="text" name="at-price" value={currPrice} onChange={e => {
+                const newPrice = Number(e.target.value);
+                setCurrPrice(newPrice);
+                setCurrAmount(portfolio[0].amount / newPrice);
+            }} />
+            <input type="text" name="receive" value={currAmount} onChange={e => {
+                const newAmount = Number(e.target.value);
+                setCurrAmount(newAmount);
+                setCurrPrice(portfolio[0].amount / newAmount);
+            }} />
+            {/* <input type="text" name="cost" value={currCost} onChange={e => {
+                setCurrCost(Number(e.target.value));
+            }} /> */}
             {orderType === 'buy' && <button className="btn-buy" onClick={() => setHistory(currPrice)}>Buy</button>}
             {orderType === 'sell' && <button className="btn-sell">Sell</button>}
         </div>

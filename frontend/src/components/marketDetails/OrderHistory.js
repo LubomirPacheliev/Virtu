f7a3158fc6f8025a8ca527e2d3dffb5fd7967beb
LubomirP <lubomirpacheliev@gmail.com>
2021-11-23 11:05:30 +0200

    making usre the compoent refreshes and Number(cost)

--- frontend/src/components/marketDetails/OrderHistory.js
+++ frontend/src/components/marketDetails/OrderHistory.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const OrderHistory = props => {
     const { history } = props;
+    const [iHistory, setiHistory] = useState(history);
+    useEffect(() => setiHistory(history), [history]);
     return ( 
         <article className="order-history">
             <div className="order-history-nav">
                 <h1>Order History</h1>
             </div>
             <div className="order-history-list">
-                {history.map((el, i) => 
+                {iHistory.map((el, i) => 
                 <p key={i}>{el.type} {el.symbol.firstSymbol} at {el.atPrice} 
-                {el.symbol.secondSymbol}, received {el.amount} {el.symbol.firstSymbol} for {el.cost.toFixed(2)} {el.symbol.secondSymbol}. </p>)}
+                {el.symbol.secondSymbol}, received {el.amount} {el.symbol.firstSymbol} for {Number(el.cost).toFixed(2)} {el.symbol.secondSymbol}. </p>)}
             </div>
         </article>
     );

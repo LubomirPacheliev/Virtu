6d38cc50dc5d603628530caa4417f32f8d1dca54
LubomirP <lubomirpacheliev@gmail.com>
2021-11-28 16:49:15 +0200

    figured out the mount paths

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,21 +1,12 @@
-// Snowpack Configuration File
-// See all supported options: https://www.snowpack.dev/reference/configuration
-
 module.exports = {
   mount: {
-    src: '/frontend',
-    public: {url: '/frontend', static: true}
+    './frontend/src': '/',
+    './frontend/public': '/'
   },
   plugins: [
     '@snowpack/plugin-react-refresh'
   ],
-  packageOptions: {
-    /* ... */
-  },
   devOptions: {
-    /* ... */
-  },
-  buildOptions: {
-    /* ... */
-  },
+    port: 4000
+  }
 };

a5043cc474f0fc3158b0a618bb579e539bed81df
LubomirP <lubomirpacheliev@gmail.com>
2021-11-29 10:49:26 +0200

    profile can't use ctx

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -2,11 +2,10 @@ import React, { useContext } from 'react';
 import { portfolioContext } from '../../portfolioContext';
 
 const Profile = () => {
-    const {portfolio} = useContext(portfolioContext);
     return (
         <div>
             <h1>Portfolio</h1>
-            <h2>I currently have: {portfolio.portfolio[0].amount} {portfolio.portfolio[0].symbol}</h2>
+            <h2></h2>
         </div>
     );
 }

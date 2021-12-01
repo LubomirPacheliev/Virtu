8ec15e4542bebe0cf8b6880f115cf2bbd154c05d
LubomirP <lubomirpacheliev@gmail.com>
2021-12-01 11:34:53 +0200

    some static html to write css with

--- src/App.js
+++ src/App.js
@@ -13,19 +13,21 @@ const App = () => {
           <h1>Virtu</h1>
           <h2>Hello, John!</h2>
       </header>
-      <aside>
-        <ul>
-          <li><Link to="/home">Home</Link></li>
-          <li><Link to="/market/SOLUSDT">Market</Link></li>
-          <li><Link to="/profile">My Portfolio</Link></li>
-        </ul>
-      </aside>
-      <Switch>
-        <Route path="/market/:symbol" component={ TradePair } />
-        <Route path="/home" component={ MarketList } />
-        <Route path="/profile" component={ Profile } />
-        <Route path="/" component={ Profile } />
-      </Switch>
+      <main>
+        <aside>
+          <ul>
+            <li><Link to="/home">Home</Link></li>
+            <li><Link to="/market/SOLUSDT">Market</Link></li>
+            <li><Link to="/profile">My Portfolio</Link></li>
+          </ul>
+        </aside>
+        <Switch>
+          <Route path="/market/:symbol" component={ TradePair } />
+          <Route path="/home" component={ MarketList } />
+          <Route path="/profile" component={ Profile } />
+          <Route path="/" component={ Profile } />
+        </Switch>
+      </main>
     </Router>
   );
 }
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,10 +2,106 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div>
+        <section className="portfolio">
+            {/* Start of USDT balance */}
             <h1>Portfolio</h1>
-            <h2></h2>
-        </div>
+            <article className="balance-usdt">
+                <h3>Your USDT Balance: <br></br><h1>$ 1,000</h1></h3>
+            </article>
+
+            {/* Start of balances cards */}
+            <h3>Balances:</h3>
+            <article className="balances">
+                <article className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" /> Bitcoin</h5>
+                        <h6 class="card-subtitle mb-2">1.45342703 BTC</h6>
+                        <p class="card-text">525.43224 USDT</p>
+                    </div>
+                </article>
+                <article className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" /> Bitcoin</h5>
+                        <h6 class="card-subtitle mb-2">1.45342703 BTC</h6>
+                        <p class="card-text">525.43224 USDT</p>
+                    </div>
+                </article>
+                <article className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" /> Bitcoin</h5>
+                        <h6 class="card-subtitle mb-2">1.45342703 BTC</h6>
+                        <p class="card-text">525.43224 USDT</p>
+                    </div>
+                </article>
+                <article className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" /> Bitcoin</h5>
+                        <h6 class="card-subtitle mb-2">1.45342703 BTC</h6>
+                        <p class="card-text">525.43224 USDT</p>
+                    </div>
+                </article>
+                <article className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" /> Bitcoin</h5>
+                        <h6 class="card-subtitle mb-2">1.45342703 BTC</h6>
+                        <p class="card-text">525.43224 USDT</p>
+                    </div>
+                </article>
+            </article>
+
+            {/* Start of recent trades */}
+            <h3>Statistic</h3>
+            <article className="statistic">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">Crypto Coin</th>
+                        <th scope="col">Earned Total</th>
+                        <th scope="col">Earned Total %</th>
+                        <th scope="col">% of Portfolio</th>
+                        <th scope="col">Number of Trades</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                    </tbody>
+                    </table>
+            </article>
+        </section>
     );
 }
  

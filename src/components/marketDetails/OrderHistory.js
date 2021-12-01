9b67f3e67c7b6860553d3765d6c41da7eadc8f94
LubomirP <lubomirpacheliev@gmail.com>
2021-12-01 20:18:57 +0200

    styled order history ( need to add tabs or something )

--- src/components/marketDetails/OrderHistory.js
+++ src/components/marketDetails/OrderHistory.js
@@ -3,7 +3,6 @@ import { portfolioContext } from '../../utils/portfolioContext';
 
 const OrderHistory = () => {
     const { history } = useContext(portfolioContext);
-    console.log(history);
 
     const returnOrderListing = order => {
         const {orderType, firstSymbol, secondSymbol, atPrice, atAmount, atCost} = order;
@@ -12,14 +11,14 @@ const OrderHistory = () => {
     }
 
     return ( 
-        <article className="order-history">
-            <div className="order-history-head">
-                <h1>your order history</h1>
-            </div>
+        <div>
+            <h1 className="order-history-h1">history</h1>
+            <article className="order-history">
             <div className="order-history-list">
-                {history.map((order, i) => returnOrderListing(order))}
+                {history.reverse().map((order, i) => returnOrderListing(order))}
             </div>
         </article>
+        </div>
     );
 }
  

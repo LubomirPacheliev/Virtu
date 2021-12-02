d3ab1e86f645a9a143d1f3d5234d5fa8da112b6d
LubomirP <lubomirpacheliev@gmail.com>
2021-12-02 12:26:54 +0200

    register functionality

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,8 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useContext, useRef, createRef } from 'react';
 import { Link, useHistory} from 'react-router-dom';
+import { FirebaseContext } from '../../utils/firebase';
 
 const Register = ({asideRef}) => {
     const history = useHistory();
+    const { app, auth } = useContext(FirebaseContext);
+    const [email, pass, repass] = [
+        createRef(),
+        createRef(),
+        createRef()
+    ];
+    
 
     useEffect(() => {
         asideRef.current.style.display = 'none';
@@ -11,14 +19,19 @@ const Register = ({asideRef}) => {
         }
     });
 
+    const registerFirebase = async () => {
+        if (pass.current.value !== repass.current.value) return alert('password need to match');
+        return await auth.createUserWithEmailAndPassword(auth.getAuth(app), email.current.value, pass.current.value);
+    }
+
     return (
         <section className="auth">
             <article className="auth-bg"></article>
             <article className="auth-form">
-                <input type="text" placeholder="your email" />
-                <input type="password" placeholder="your password" />
-                <input type="password" placeholder="confirm your password" />
-                <button className="btn-login">register</button>
+                <input type="text" placeholder="your email" ref={email} />
+                <input type="password" placeholder="your password" ref={pass} />
+                <input type="password" placeholder="confirm your password" ref={repass} />
+                <button className="btn-login" onClick={registerFirebase}>register</button>
                 <Link to="/login">You've already registered?</Link>
             </article>
             <a className="auth-goback" onClick={() => history.push('/profile')}>Go Back</a>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,16 @@
 import React, { createContext } from 'react';
-import * as app from 'firebase/app';
+import { initializeApp } from 'firebase/app';
+import * as auth from 'firebase/auth';
+import { config } from './readEnv';
 
 const FirebaseContext = createContext(null);
 export { FirebaseContext };
 
 export default ({ children }) => {
-    app.initializeApp({
-      apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-      authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-      projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-      storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    });
+    const app = initializeApp(config);
+    auth.initializeAuth(app);
     return (
-      <FirebaseContext.Provider value={ app }>
+      <FirebaseContext.Provider value={ { app, auth } }>
         { children }
       </FirebaseContext.Provider>
     );

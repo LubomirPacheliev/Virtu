39504d16d4e6c831fdb0b4a603270781ccba5b19
LubomirP <lubomirpacheliev@gmail.com>
2021-12-02 17:48:56 +0200

    adding react cookies

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,6 +2,7 @@ import React, { createContext } from 'react';
 import { initializeApp } from 'firebase/app';
 import * as auth from 'firebase/auth';
 import { config } from './readEnv';
+import { CookiesProvider } from 'react-cookie';
 
 const FirebaseContext = createContext(null);
 export { FirebaseContext };
@@ -11,7 +12,9 @@ export default ({ children }) => {
     auth.initializeAuth(app);
     return (
       <FirebaseContext.Provider value={ { app, auth } }>
-        { children }
+        <CookiesProvider>
+          { children }
+        </CookiesProvider>
       </FirebaseContext.Provider>
     );
 }

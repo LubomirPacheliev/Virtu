0cc79bcb280290b3f849ccabeea4cc49dae57aaf
LubomirP <lubomirpacheliev@gmail.com>
2021-12-06 17:23:48 +0200

    dynamic usdt balance data

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { FirebaseContext } from '../../utils/firebase';
+import { useCookies } from 'react-cookie';
 
 const Profile = () => {
+    const [cookies, setCookies] = useCookies();
+    const [balance, setBalance] = useState(1000);
+    const { firestore, firestoreInstance } = useContext(FirebaseContext);
+    const email = cookies.email;
+
+    useEffect(async () => {
+        const docRef = await firestore.getDoc(firestore.doc(firestoreInstance, 'assets/' + email));
+        const docVal = await docRef.data();
+        console.log(docVal);
+        setBalance(docVal.capital);
+    }, []);
+
     return (
         <section className="portfolio">
             {/* Start of USDT balance */}
             <h1 className="h1-portfolio">portfolio</h1>
             <article className="balance-usdt">
-                <h3 className="one-em">your balance <br></br><h1>1,000.00 USDT</h1></h3>
+                <h3 className="one-em">your balance <br></br><h1>{balance} USDT</h1></h3>
             </article>
 
             {/* Start of balances cards */}

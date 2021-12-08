6d6c5e7fd138fb7e5b51f4d5ceba1a8daa90763d
LubomirP <lubomirpacheliev@gmail.com>
2021-12-08 15:34:45 +0200

    seperate statistics component

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Balance from './Balance.js';
 import Cards from './Cards.js';
+import Statistics from './Statistics.js';
 
 const Profile = () => {
 
@@ -8,59 +9,7 @@ const Profile = () => {
         <section className="portfolio">
             <Balance />
             <Cards />
-
-            {/* Start of recent trades */}
-            <article className="statistic">
-                <h3 className="one-em">statistics</h3>
-                <table class="table">
-                    <thead>
-                        <tr>
-                        <th scope="col">CRYPTO COIN</th>
-                        <th scope="col">EARNED TOTAL</th>
-                        <th scope="col">EARNED TOTAL %</th>
-                        <th scope="col">% OF PORTFOLIO</th>
-                        <th scope="col">NUMBER OF TRADES</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
-                            <td>1.45903210</td>
-                            <td>18.5</td>
-                            <td>56</td>
-                            <td>15</td>
-                        </tr>
-                        <tr>
-                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
-                            <td>1.45903210</td>
-                            <td>18.5</td>
-                            <td>56</td>
-                            <td>15</td>
-                        </tr>
-                        <tr>
-                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
-                            <td>1.45903210</td>
-                            <td>18.5</td>
-                            <td>56</td>
-                            <td>15</td>
-                        </tr>
-                        <tr>
-                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
-                            <td>1.45903210</td>
-                            <td>18.5</td>
-                            <td>56</td>
-                            <td>15</td>
-                        </tr>
-                        <tr>
-                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
-                            <td>1.45903210</td>
-                            <td>18.5</td>
-                            <td>56</td>
-                            <td>15</td>
-                        </tr>
-                    </tbody>
-                    </table>
-            </article>
+            <Statistics />
         </section>
     );
 }
---
+++ src/components/profile/Statistics.js
@@ -0,0 +1,59 @@
+import React from 'react';
+
+const Statistics = () => {
+    return (
+        <article className="statistic">
+            <h3 className="one-em">statistics</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th scope="col">CRYPTO COIN</th>
+                        <th scope="col">EARNED TOTAL</th>
+                        <th scope="col">EARNED TOTAL %</th>
+                        <th scope="col">% OF PORTFOLIO</th>
+                        <th scope="col">NUMBER OF TRADES</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                        <tr>
+                            <td><img src="http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png" />Bitcoin</td>
+                            <td>1.45903210</td>
+                            <td>18.5</td>
+                            <td>56</td>
+                            <td>15</td>
+                        </tr>
+                    </tbody>
+                    </table>
+        </article>
+    );
+}
+ 
+export default Statistics;

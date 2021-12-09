20debb05759bcb96f7d03b706fd2a4ba6de9f066
LubomirP <lubomirpacheliev@gmail.com>
2021-12-09 15:20:42 +0200

    react spinners & reusable cards comp

--- src/components/profile/Cards.js
+++ src/components/profile/Cards.js
@@ -1,48 +1,6 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { FirebaseContext } from '../../utils/firebase';
-import { useCookies } from 'react-cookie';
-import fetch from 'isomorphic-fetch';
+import React from 'react';
 
-const Cards = () => {
-    const { firestore, firestoreInstance } = useContext(FirebaseContext);
-    const [ cookies ] = useCookies();
-    const [cards, setCards] = useState([]);
-    const email = cookies.email;
-    const defaultCards = [
-        { img: "http://pngimg.com/uploads/bitcoin/bitcoin_PNG7.png", coin: 'Bitcoin', amount: 0, usdtValue: 0, symbol: 'BTC' },
-        { img: "https://logodownload.org/wp-content/uploads/2021/01/bitcoin-cash-logo-2-768x768.png", coin: 'Bitcoin Cash', amount: 0, usdtValue: 0, symbol: 'BCH'},
-        { img: "https://vectorified.com/image/ethereum-logo-vector-13.png", coin: 'Etherium', amount: 0, usdtValue: '0', symbol: 'ETH' },
-        { img: "https://cryptobuyersclub.co.uk/wp-content/uploads/2020/07/Stellar-XLM-Logo.png", coin: 'Lumens', amount: '0', usdtValue: '0', symbol: 'XLM' },
-        { img: "https://static.coinpaprika.com/coin/sol-solana/logo.png?rev=10608559", coin: 'Solana', amount: '0', usdtValue: '0', symbol: 'SOL' }
-    ];
-    
-    useEffect(async () => {
-        const docs = await firestore.getDocs(firestore.collection(firestoreInstance, `assets/${email}/assets`));
-        const assetsRef = docs.docs;
-        const assets = [];
-        if (assetsRef.length < 5) {
-            for (let i = assetsRef.length; i < 5; i++) {
-                assets.push(defaultCards[i]);
-            }
-         }
-        assetsRef.map(async (asset, i) => {
-            const symbol = asset.id;
-            const assetVal = await asset.data();
-            const ticker =  await fetch('https://api.binance.com/api/v3/ticker/24hr?symbol=' + symbol.toUpperCase() + 'USDT');
-            const parsedTicker = await ticker.json();
-            const usdtValue = Number(parsedTicker.lastPrice) * assetVal.amount;
-            const returnVal = {
-                img: "https://vectorified.com/image/ethereum-logo-vector-13.png", 
-                coin: symbol, 
-                symbol,
-                amount: Number(assetVal.amount).toFixed(7), 
-                usdtValue: Number(usdtValue).toFixed(7)
-            };
-            assets.unshift(returnVal);
-            if (assets.length === 5) setCards(assets);
-        });
-    }, []);
-
+const Cards = ({cards}) => {
     return (
         <div>
             <h3 className="one-em">balances</h3>

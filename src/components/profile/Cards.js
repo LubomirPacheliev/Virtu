38924e1ef1840a29d09e89442a20bf1a2d0b1f67
LubomirP <lubomirpacheliev@gmail.com>
2021-12-12 11:09:55 +0200

    switching between displayed assets

--- src/components/profile/Cards.js
+++ src/components/profile/Cards.js
@@ -1,18 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Cards = ({cards}) => {
+    const [load, setLoad] = useState(0);
     const backwards = '<';
     const forwards = '>';
     return (
         <div className="cards-div">
             <article className="balances-nav">
                 <h3 className="one-em">balances</h3>
-                <button className="backward">{backwards}</button>
-                <button className="forward">{forwards}</button>
+                <button className="backward" onClick={() => setLoad(lastLoad => lastLoad - 4)}>{backwards}</button>
+                <button className="forward" onClick={() => setLoad(lastLoad => lastLoad + 4)}>{forwards}</button>
             </article>
             <article className="balances">
                 {cards.map((card, i) => {
-                    if (i < 5) return returnCard(card, i);
+                    if (i >= load && i < load + 5) return returnCard(card, i);
                 })}
             </article>
         </div>
@@ -20,7 +21,7 @@ const Cards = ({cards}) => {
 }
 
 const returnCard = (card, i) => {
-    return (
+    return ( 
         <article className="card" key={i}>
             <div className="card-body">
                 <h5 className="card-title"><img src={card.img}/> {card.coin}</h5>

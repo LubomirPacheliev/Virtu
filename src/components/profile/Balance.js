98b06f31a014a3eed13459b5936fd7fb20f231d0
LubomirP <lubomirpacheliev@gmail.com>
2021-12-19 16:31:11 +0200

    balance component suitable for guest usability

--- src/components/profile/Balance.js
+++ src/components/profile/Balance.js
@@ -1,17 +1,23 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { FirebaseContext } from '../../utils/firebase';
 import { useCookies } from 'react-cookie';
+import { GuestCtx } from '../../utils/GuestCtx';
 
 const Balance = () => {
     const [cookies, setCookies] = useCookies();
     const [balance, setBalance] = useState(1000);
     const { firestore, firestoreInstance } = useContext(FirebaseContext);
+    const { guestUSDT } = useContext(GuestCtx);
     const email = cookies.email;
 
     useEffect(async () => {
-        const docRef = await firestore.getDoc(firestore.doc(firestoreInstance, 'assets/' + email));
-        const docVal = await docRef.data();
-        setBalance(docVal.capital);
+        if (typeof email !== 'undefined') {
+            const docRef = await firestore.getDoc(firestore.doc(firestoreInstance, 'assets/' + email));
+            const docVal = await docRef.data();
+            setBalance(docVal.capital);
+        } else {
+            setBalance(guestUSDT);
+        }
     }, []);
 
     return (
